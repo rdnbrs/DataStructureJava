d0d51861e473fbc08a80f8fef1239d59ee5418ee
unknown <barisrdn@gmail.com>
2023-03-08 14:59:22 +0300

    KokoEatingBananas better

--- src/com/rdnbrs/question/questions/KokoEatingBananas.java
+++ src/com/rdnbrs/question/questions/KokoEatingBananas.java
@@ -11,56 +11,26 @@ public class KokoEatingBananas {
         System.out.println(minEatingSpeed(new int[]{30, 11, 23, 4, 20}, 6));
     }
 
-    /**
-     * Time Limit Exceeded
-     */
-//    public static int minEatingSpeed(int[] piles, int h) {
-//        int median = 0, sum = 0, max = Integer.MIN_VALUE;
-//
-//        for (int item : piles) {
-//            sum += item;
-//            if (item > max)
-//                max = item;
-//        }
-//
-//        median = (int) Math.ceil(sum / (h * 1.0f));
-//
-//        while (median < max) {
-//            sum = 0;
-//            for (int item : piles) {
-//                sum += Math.ceil(1.0f * item / median);
-//            }
-//
-//            if (sum == h)
-//                break;
-//            else median++;
-//        }
-//
-//        return median;
-//    }
     public static int minEatingSpeed(int[] piles, int h) {
-        int low = 1;
-        int high = Integer.MIN_VALUE;
-        for (int item : piles) {
-            high = Math.max(high, item);
-        }
-        while (low < high) {
-            int mid = low + (high - low) / 2;
-            if (blackbox(mid, piles, h)) {
-                high = mid;
-            } else
-                low = mid + 1;
-        }
-        return low;
-    }
+        int median = 1, max = Integer.MIN_VALUE;
 
-    public static boolean blackbox(int maxpiles, int[] piles, int h) {
-        int hours = 0;
-        for (int i : piles) {
-            int time = i / maxpiles;
-            hours += time;
-            if (i % maxpiles != 0) hours++;
+        for (int item : piles) {
+            if (item > max) max = item;
         }
-        return hours <= h;
+
+        while (median < max) {
+            int hours = 0;
+            int mid = median + (max - median) / 2;
+            for (int i : piles) {
+                int time = i / mid;
+                hours += time;
+                if (i % mid != 0) hours++;
+            }
+
+            if (hours <= h) max = mid;
+            else median = mid + 1;
+        }
+
+        return median;
     }
 }
