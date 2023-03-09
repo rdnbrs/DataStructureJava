fbbdf723f32028f8f4da18872cafc8cd6844fb3a
unknown <barisrdn@gmail.com>
2023-03-09 21:47:21 +0300

    LinkedListCycleII

---
+++ src/com/rdnbrs/question/questions/LinkedListCycleII.java
@@ -0,0 +1,48 @@
+package com.rdnbrs.question.questions;
+
+import com.rdnbrs.question.questions.models.ListNode;
+
+public class LinkedListCycleII {
+
+    /**
+     * https://leetcode.com/problems/linked-list-cycle-ii/
+     */
+
+    public static void main(String[] args) {
+        ListNode node1 = new ListNode(3);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(0);
+        ListNode node4 = new ListNode(-4);
+        ListNode node5 = new ListNode(5);
+        ListNode node6 = new ListNode(6);
+
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        node5.next = node6;
+        node6.next = node5;
+
+        System.out.println(detectCycle(node1).val);
+    }
+
+    public static ListNode detectCycle(ListNode head) {
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                break;
+            }
+        }
+        if (fast == null || fast.next == null) {
+            return null;
+        }
+        slow = head;
+        while (slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
+    }
+}
